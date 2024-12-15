829094c6daff4eed6b0da6bc866d6eb0831f4308
Aditya <itsmeaditya3254@gmail.com>
2024-12-15 23:32:40 +0530

    first commit

---
+++ realtime-editor/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+      <img
+                    className="homePageLogo"
+                    src="/Code-Pulse.jpg"
+                    alt="code-sync-logo"
+                />
+      <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
